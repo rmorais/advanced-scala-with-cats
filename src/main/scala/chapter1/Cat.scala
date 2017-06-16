f6f78ac268e67937670325c02176893d92b66c2b
Rui Morais <rui.morais@ovoenergy.com>
2017-06-16 16:52:20 +0300

    create catShow

--- src/main/scala/chapter1/Cat.scala
+++ src/main/scala/chapter1/Cat.scala
@@ -1,7 +1,22 @@
 package chapter1
 
+import cats.Show
+import cats.instances.int._
+import cats.instances.string._
+import cats.syntax.show._
+
 final case class Cat(
     name: String,
     age: Int,
     color: String
 )
+
+
+object Cat {
+  implicit val catShow = Show.show[Cat] { cat =>
+    val name  = cat.name.show
+    val age   = cat.age.show
+    val color = cat.color.show
+    s"$name is a $age year-old $color cat.(Using cats.Show)"
+  }
+}
--- src/main/scala/chapter1/printable/Runner.scala
+++ src/main/scala/chapter1/printable/Runner.scala
@@ -3,10 +3,12 @@ package chapter1.printable
 import chapter1.Cat
 import PrintableInstances._
 import PrintableSyntax._
+import cats.syntax.show._
 
 object Runner extends App {
 
   val cat = Cat(name = "Tioma", age = 8, color = "gray")
 
   cat.print
+  println(cat.show)
 }
