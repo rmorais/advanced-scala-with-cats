3c55f99987e5360e0c53da3df4229fa7bc5125c9
Rui Morais <rui.morais@ovoenergy.com>
2017-06-16 16:50:45 +0300

    fix format method by calling the format method on cat fields instead of default toString

--- src/main/scala/chapter1/printable/PrintableInstances.scala
+++ src/main/scala/chapter1/printable/PrintableInstances.scala
@@ -13,6 +13,11 @@ object PrintableInstances {
   }
 
   implicit val PrintableCat = new Printable[Cat] {
-    override def format(value: Cat): String = s"${value.name} is a ${value.age} year-old ${value.color} cat."
+    override def format(cat: Cat): String = {
+      val name = Printable.format(cat.name)
+      val age = Printable.format(cat.age)
+      val color = Printable.format(cat.color)
+      s"$name is a $age year-old $color cat."
+    }
   }
 }
