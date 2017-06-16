fd9dc9888fa55f0ec7eff8264016856ad43f94f3
Rui Morais <rui.morais@ovoenergy.com>
2017-06-16 17:29:08 +0300

    add typesafe equality for Cat using cates.Eq

--- src/main/scala/chapter1/Cat.scala
+++ src/main/scala/chapter1/Cat.scala
@@ -4,6 +4,8 @@ import cats.Show
 import cats.instances.int._
 import cats.instances.string._
 import cats.syntax.show._
+import cats.syntax.eq._
+import cats.Eq
 
 final case class Cat(
     name: String,
@@ -11,12 +13,17 @@ final case class Cat(
     color: String
 )
 
-
 object Cat {
   implicit val catShow = Show.show[Cat] { cat =>
-    val name  = cat.name.show
-    val age   = cat.age.show
+    val name = cat.name.show
+    val age = cat.age.show
     val color = cat.color.show
     s"$name is a $age year-old $color cat.(Using cats.Show)"
+  }
+
+  implicit val catEq = Eq.instance[Cat] { (cat1, cat2) =>
+    (cat1.name === cat2.name) &&
+    (cat1.color === cat2.color) &&
+    (cat1.age === cat2.age)
   }
 }
