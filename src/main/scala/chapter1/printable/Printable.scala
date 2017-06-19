ea53018121a1e099d2c7355b9aee4efe4db74fb3
Rui Morais <rui.morais@ovoenergy.com>
2017-06-19 17:11:13 +0300

    add contramap method to Printable

--- src/main/scala/chapter1/printable/Printable.scala
+++ src/main/scala/chapter1/printable/Printable.scala
@@ -4,6 +4,14 @@ trait Printable[A] {
 
   def format(value: A): String
 
+  def contramap[B](func: B => A): Printable[B] = {
+    val self = this
+
+    new Printable[B] {
+      override def format(value: B): String = self.format(func(value))
+    }
+  }
+
 }
 
 
