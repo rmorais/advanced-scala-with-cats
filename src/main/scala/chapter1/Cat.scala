5e07513dbb42fbfce98f1336da555a90aca33678
Rui Morais <rui.morais@ovoenergy.com>
2018-10-26 12:56:31 +0100

    add Eq instance for Cat

--- src/main/scala/chapter1/Cat.scala
+++ src/main/scala/chapter1/Cat.scala
@@ -1,6 +1,10 @@
 package chapter1
 
 import PrintableInstances._
+import cats.Eq
+import cats.syntax.eq._
+import cats.instances.string._
+import cats.instances.int._
 
 final case class Cat(name: String, age: Int, color: String)
 
@@ -13,6 +17,12 @@ object Cat {
       s" $name is a $age year-old $color cat."
     }
   }
+
+  implicit val eqCat: Eq[Cat] = Eq.instance[Cat] { (x, y) =>
+    (x.name === y.name) &&
+    (x.age === y.age) &&
+    (x.color === y.color)
+  }
 }
 
 object CatRunner extends App {
@@ -23,4 +33,17 @@ object CatRunner extends App {
   //using syntax
   import PrintableSyntax._
   cat.print
+
+  val cat1 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  println(cat1 === cat2)
+  println(cat1 =!= cat2)
+
+  import cats.instances.option._
+  println(optionCat1 === optionCat2)
+  println(optionCat1 =!= optionCat2)
 }
