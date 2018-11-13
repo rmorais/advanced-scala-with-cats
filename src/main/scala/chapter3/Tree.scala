fb6b619adf5837a2b60fb28686fc4794eee5fea9
Rui Morais <rui.morais@ovoenergy.com>
2018-11-13 09:44:02 +0000

    add Functor instance for Tree

--- src/main/scala/chapter3/Tree.scala
+++ src/main/scala/chapter3/Tree.scala
@@ -9,32 +9,22 @@ final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 final case class Leaf[A](value: A) extends Tree[A]
 
-object Tree {
 
+object Tree {
   implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
-    override def map[A, B](fa: Tree[A])(f: (A) => B): Tree[B] = fa match {
-      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
-      case Leaf(v) => Leaf(f(v))
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = {
+      fa match {
+        case Leaf(v) => Leaf(f(v))
+        case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+      }
     }
   }
 
-  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] = {
+  //smart constructors to fix type inference issues
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
     Branch(left, right)
-  }
 
-  def leaf[A](value: A): Tree[A] = Leaf(value)
-}
+  def leaf[A](value: A): Tree[A] =
+    Leaf(value)
 
-object TreeRunner extends App {
-
-  import Tree._
-  val leafInstance = leaf(1)
-
-  val branchInstance = branch(Leaf(2), Leaf(3))
-
-  val f = (x: Int) => x + 1
-
-  assert(leafInstance.map(_ * 4 ) == leaf(4))
-
-  assert(branchInstance.map(_ * 4) == branch(leaf(8), leaf(12)))
 }
