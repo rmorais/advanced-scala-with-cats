636eee3cd9f0288019fc0d1755d4ed837743f09a
Rui Morais <rui.morais@ovoenergy.com>
2018-11-13 10:00:26 +0000

    define Printable for Box[A]

--- src/main/scala/chapter3/Box.scala
+++ src/main/scala/chapter3/Box.scala
@@ -1,5 +1,14 @@
 package chapter3
 
-class Box {
+import chapter1.Printable
 
+final case class Box[A](value: A)
+
+object Box {
+
+  def traditionalPrintableBox[A](implicit printable: Printable[A]): Printable[Box[A]] = new Printable[Box[A]] {
+    override def format(box: Box[A]): String = printable.format(box.value)
+  }
+
+  implicit def printableBox[A](implicit printable: Printable[A]): Printable[Box[A]] = printable.contramap(_.value)
 }
