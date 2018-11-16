ac40f2b11f53859c7665e6b2f44357d6a4fb56c5
Rui Morais <rui.morais@ovoenergy.com>
2018-11-16 09:50:28 +0000

    implement imap for Codec

--- src/main/scala/chapter3/Codec.scala
+++ src/main/scala/chapter3/Codec.scala
@@ -1,13 +1,13 @@
 package chapter3
 
 trait Codec[A] {
+  self =>
   def encode(value: A): String
   def decode(value: String): A
   def imap[B](decoder: A => B, encoder: B => A): Codec[B] = new Codec[B] {
-    val self = this
     override def encode(value: B): String = self.encode(encoder(value))
 
-    override def decode(value: String): B = decoder(this.decode(value))
+    override def decode(value: String): B = decoder(self.decode(value))
   }
 }
 
@@ -15,6 +15,11 @@ object Codec {
   def encode[A](value: A)(implicit codec: Codec[A]): String =codec.encode(value)
   def decode[A](value: String)(implicit codec: Codec[A]): A = codec.decode(value)
 
+
+}
+
+
+object CodecInstances {
   implicit val stringCodec: Codec[String] = new Codec[String] {
     override def encode(value: String): String = value
 
@@ -25,5 +30,13 @@ object Codec {
   implicit val intCodec: Codec[Int] = stringCodec.imap(_.toInt, _.toString)
 
   implicit val booleanCodec: Codec[Boolean] = stringCodec.imap(_.toBoolean, _.toString)
+
+  implicit val doubleCodec: Codec[Double] = stringCodec.imap(_.toDouble, _.toString)
+
+  implicit def boxCodec[A](implicit ca: Codec[A]): Codec[Box[A]] = new Codec[Box[A]] {
+    override def encode(box: Box[A]): String = ca.encode(box.value)
+
+    override def decode(box: String): Box[A] = Box(ca.decode(box))
+  }
 
 }
