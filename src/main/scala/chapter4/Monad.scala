629ba2b76322ffbd4e7ba7b31eba16d161e21059
Rui Morais <rui.morais@ovoenergy.com>
2018-12-07 11:38:01 +0000

    add Monad instance for Id

--- src/main/scala/chapter4/Monad.scala
+++ src/main/scala/chapter4/Monad.scala
@@ -1,5 +1,7 @@
 package chapter4
 
+import cats.Id
+
 trait Monad[F[_]] {
   def pure[A](a: A): F[A]
 
@@ -7,4 +9,16 @@ trait Monad[F[_]] {
 
   def map[A,B](value: F[A])(f: A => B): F[B] =
     flatMap(value)(f andThen pure)
+}
+
+
+object instances {
+
+  implicit val idMonad: Monad[Id] = new Monad[Id] {
+    override def pure[A](a: A): Id[A] = a
+
+    override def flatMap[A, B](value: Id[A])(f: A => Id[B]): Id[B] = f(value)
+
+    override def map[A, B](value: Id[A])(f: A => B): Id[B] = f(value)
+  }
 }
